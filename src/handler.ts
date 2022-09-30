cae70476f0b3c85f1792b7fc71d7370aa84ddca1
Juan Villela <juan@villela.co>
2022-09-30 17:38:38 -0400

    chore: Remove auth from callback route.

--- src/handler.ts
+++ src/handler.ts
@@ -37,30 +37,12 @@ export const handleAuth = async (ctx: Context) => {
 };
 
 export const handleCallback = async (ctx: Context) => {
-  const authKey = ctx.env.AUTH_KEY;
   const request = ctx.req;
 
   try {
     const { searchParams } = new URL(request.url);
     const code = searchParams.get('code');
     const state = searchParams.get('state');
-    const key = searchParams.get('key');
-
-    if (!key) {
-      ctx.status(400);
-      return ctx.json({
-        error: "Missing 'Key' header.",
-        version,
-      });
-    }
-    if (key !== authKey) {
-      ctx.status(400);
-      return ctx.json({
-        error: "You're not authorized to access this API.",
-        version,
-      });
-    }
-
     const id = await addState(ctx.env, code, state);
 
     ctx.status(200);
