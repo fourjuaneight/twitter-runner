79b1abd4ac6f8631cce8ac58a5d66dc58475daac
Juan Villela <juan@villela.co>
2022-10-03 16:53:33 -0400

    chore: Add better comments.

--- src/app.ts
+++ src/app.ts
@@ -9,12 +9,16 @@ import {
 
 const app = new Hono();
 
+// generate twitter auth url and save code + state to DB
 app.get('/auth', async ctx => handleAuth(ctx));
 
+// save authorized code + sate and access token, then return token
 app.get('/', async ctx => handleCallback(ctx));
 
+// get latest access token by user
 app.get('/access', async ctx => handleAccess(ctx));
 
+// refresh access token.
 app.get('/refresh', async ctx => handleRefresh(ctx));
 
 export default app;
