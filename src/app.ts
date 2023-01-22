336e47381c75dff571f9a59f9a5cb51d474869ec
Juan Villela <juan@villela.co>
2023-01-22 12:46:54 -0500

    fix!: Change callback endpoint with literal route.

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ const app = new Hono();
 app.get('/auth', async (ctx) => handleAuth(ctx));
 
 // save authorized code + sate and access token, then return token
-app.get('/', async (ctx) => handleCallback(ctx));
+app.get('/callback', async (ctx) => handleCallback(ctx));
 
 // get latest access token by user
 app.get('/access', async (ctx) => handleAccess(ctx));
